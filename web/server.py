b6d573f5a26f5aa20698370c525614fc16b22c23
daniela08 <danielacauti@gmail.com>
2020-04-20 17:15:53 -0500

    files committed

--- web/server.py
+++ web/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, request, session, Response, redirect
+from flask import Flask, render_template, request, session, Response, redirect
 from database import connector
 from model import entities
 import json
@@ -9,11 +9,45 @@ engine = db.createEngine()
 
 app = Flask(__name__)
 
+@app.route('/saludar')
+def saludar():
+    return "HOLA2"
+
+
+@app.route('/esprimo/<numero>')
+def es_primo(numero):
+    num = 0
+    if int(numero) >1:
+        for i in range(2, int(numero)):
+            if (int(numero) % i == 0):
+                num += 1
+        return str(num == 0)
+    return str(int(numero) == 2)
+
+
+@app.route('/palindrome/<palabra>')
+def es_palindromo(palabra):
+    letras = list(palabra)
+    n = len(letras)
+    reverso = ""
+    for i in range (1, n + 1):
+        reverso += letras[n - i]
+    return str(reverso == palabra)
+
+
+@app.route('/multiplo/<numero1>/<numero2>')
+def es_multiplo(numero1, numero2):
+    return str(int(numero1) % int(numero2) == 0)
+
 
 @app.route('/static/<content>')
 def static_content(content):
     return render_template(content)
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "NOT FOUND", 404
 
 
 
