fe10bf6983a1815d5d697e973fe38b587f126857
daniela08 <danielacauti@gmail.com>
2020-04-23 15:41:19 -0500

    committed

--- web/server.py
+++ web/server.py
@@ -9,10 +9,6 @@ engine = db.createEngine()
 
 app = Flask(__name__)
 
-@app.route('/saludar')
-def saludar():
-    return "HOLA2"
-
 
 @app.route('/esprimo/<numero>')
 def es_primo(numero):
@@ -23,6 +19,50 @@ def es_primo(numero):
                 num += 1
         return str(num == 0)
     return str(int(numero) == 2)
+
+
+@app.route('/static/<content>')
+def static_content(content):
+    return render_template(content)
+
+
+@app.route('/create_user/<nombre>/<apellido>/<passwd>/<uname>')
+def create_user(nombre, apellido, passwd, uname):
+    #Crear un objeto (instancia de una entidad)
+    user = entities.User(
+        name = nombre,
+        fullname = apellido,
+        password = passwd,
+        username = uname
+    )
+
+    #Guardar el objeto en la capa de ersistencia
+    db_session = db.getSession(engine)
+    db_session.add(user)
+    db_session.commit()
+
+    return "User created!"
+
+@app.route('/read_users')
+def read_users():
+    db_session = db.getSession(engine)
+    respuesta = db_session.query(entities.User)
+    users = respuesta[:]
+    i = 0
+    for user in users:
+        print(i, "NAME:\t", user.name)
+        i += 1
+        return "Users read!"
+
+
+if __name__ == '__main__':
+    app.secret_key = ".."
+    app.run(port=8080, threaded=True, host=('127.0.0.1'))
+
+
+@app.route('/saludar')
+def saludar():
+    return "HOLA2"
 
 
 @app.route('/palindrome/<palabra>')
@@ -40,17 +80,6 @@ def es_multiplo(numero1, numero2):
     return str(int(numero1) % int(numero2) == 0)
 
 
-@app.route('/static/<content>')
-def static_content(content):
-    return render_template(content)
-
-
 @app.errorhandler(404)
 def page_not_found(e):
     return "NOT FOUND", 404
-
-
-
-if __name__ == '__main__':
-    app.secret_key = ".."
-    app.run(port=8080, threaded=True, host=('127.0.0.1'))
